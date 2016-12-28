edf9e13387ab18765f69b41c69c52534ef703fa8
loganrice <loganrice72@gmail.com>
2016-12-27 19:59:37 -0800

    ch2 programming c excersises

---
+++ books/programming_in_c/ch2/prog2.c
@@ -0,0 +1,15 @@
+/* Write a program that subtracts the value 15 from 87
+ * and displays the result.
+ * */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int num1, num2;
+  num1 = 87;
+  num2 = 15;
+
+  printf("%i minus %i is %i \n", num1, num2, num1 - num2);
+
+}
